fd9cf3d6be0af9eb547b20c3e86841d5421e7010
andrewrennie29 <andrew.rennie@me.com>
2015-03-06 06:43:04 +0000

    updated start a meeting

--- app/assets/javascripts/application.js~
+++ app/assets/javascripts/application.js~
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require moment
-
 //= require bootstrap-sprockets
 //= require bootstrap-datetimepicker
 //= require_tree .
--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,2 +1,19 @@
 class AttendeesController < ApplicationController
+
+  def index
+
+    render :index
+
+  end
+
+  def create
+    @attendee = Attendee.create(:meetingid => session[:activemeetingid], :dept => 'GM')
+    @attendee = Attendee.create(:meetingid => session[:activemeetingid], :dept => 'Inbound Ops')
+    @attendee = Attendee.create(:meetingid => session[:activemeetingid], :dept => 'Outbound Ops')
+    @attendee = Attendee.create(:meetingid => session[:activemeetingid], :dept => 'HR')
+    @attendee = Attendee.create(:meetingid => session[:activemeetingid], :dept => 'Finance')
+    @attendee = Attendee.create(:meetingid => session[:activemeetingid], :dept => 'PPT')
+    @attendee = Attendee.create(:meetingid => session[:activemeetingid], :dept => 'Others')
+  end
+
 end
